1ada129b602cfe51a9d0961e691ec6414d21e39c
amyjoscelyn <amy.joscelyn@flatironschool.com>
2016-02-01 15:03:49 -0500

    setting up .xcdatamodeld with core story entities

---
+++ Dream/Dream/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dream
+//
+//  Created by Amy Joscelyn on 2/1/16.
+//  Copyright © 2016 Amy Joscelyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
